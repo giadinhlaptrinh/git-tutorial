bedb75260e33adb6e47fbe1b0085570ebc97ebc0
Hung Pham <truonghungit@gmail.com>
2022-12-22 13:41:11 +0700

    add deployment script

--- next.config.js
+++ next.config.js
@@ -1,7 +1,28 @@
 /** @type {import('next').NextConfig} */
+
+const productionBaseUrl = "git-tutorial";
+const BASE_URL = productionBaseUrl || "";
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  basePath: BASE_URL,
+  env: {
+    BASE_URL,
+  },
+  async redirects() {
+    if (BASE_URL) {
+      return [
+        {
+          source: "/",
+          destination: BASE_URL,
+          basePath: false,
+          permanent: false,
+        },
+      ];
+    }
+    return [];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,10 +6,40 @@ import "../styles/lesson-content.scss";
 
 import type { AppProps } from "next/app";
 import { AppLayout } from "../layouts/app-layout";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppLayout>
+      <Head>
+        <link
+          rel="icon"
+          type="image/x-icon"
+          href={`${process.env.BASE_URL}/favicon.ico`}
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href={`${process.env.BASE_URL}/apple-touch-icon.png`}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href={`${process.env.BASE_URL}/favicon-32x32.png`}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href={`${process.env.BASE_URL}/favicon-16x16.png`}
+        />
+        <link
+          rel="manifest"
+          href={`${process.env.BASE_URL}/site.webmanifest`}
+        />
+      </Head>
+
       <Component {...pageProps} />
     </AppLayout>
   );
